292cf756e9b897f2343b1a7bb14f2362c79aefe1
profissional09 <127982510+EZEQUIELFILHO383@users.noreply.github.com>
2023-08-11 19:56:11 -0300

    https://github.com/EZEQUIELFILHO383/todo-list.git

---
+++ scripts.js
@@ -0,0 +1,65 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function addNewTask() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  })
+
+  input.value = ''
+
+  showTask()
+}
+
+function showTask() {
+  let novaLi = ''
+
+  // ['comprar café', 'estudar programação']
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi =
+      novaLi +
+      `
+
+        <li class="task ${item.concluida && 'done'}">
+            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+        </li>
+        
+        `
+  })
+
+  listaCompleta.innerHTML = novaLi
+
+  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+  showTask()
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1)
+
+  showTask()
+}
+
+function recarregarTarefas() {
+  const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+  if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+  }
+
+  showTask()
+}
+
+recarregarTarefas()
+button.addEventListener('click', addNewTask)
